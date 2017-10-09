3eaf7e917941f67330f1deb3035b9ad2872b75d4
Ekarat Rattagan <pokekarat@gmail.com>
2017-10-09 21:57:02 +0700

    Update Client.cs

--- Game/Week9_Client_Server/Client.cs
+++ Game/Week9_Client_Server/Client.cs
@@ -6,34 +6,59 @@ using System.Text;
 using System;
 using System.IO;
 
-public class Client : MonoBehaviour
+public class Main : MonoBehaviour
 {
 	StreamWriter writer;
 	StreamReader reader;
 	NetworkStream stream;
+	Player player;
+	TcpClient client;
 	string id;
 	// Use this for initialization
 	void Start () {
-		print("Connection");
-		TcpClient client = new TcpClient ("127.0.0.1", 16000);
-		stream = client.GetStream();
+		player = new Player ();
+		player.name = "ekarat";
+
+		print ("Connection");
+		client = new TcpClient ("127.0.0.1", 16000);
+
+		stream = client.GetStream ();
 		stream.ReadTimeout = 10;
-		//stream.WriteTimeout = 2;
 		if (stream.CanRead) {
-			writer = new StreamWriter(stream);
+			writer = new StreamWriter (stream);
 			reader = new StreamReader (stream);
-			print("Writer created");
-			readData ();
+			Debug.Log (reader.ReadLine ());
 		}
 	}
 
-	void readData(){
-		if (stream.CanRead) {
-			Debug.Log(reader.ReadLine ());
-			writer.WriteLine("hi");
-			writer.Flush ();
+	//60 fps
+	int s = 0;
+	void Update(){
 
+		//Send data every 1 second
+		if (s % 60 == 0) {
+			player.x = 1.5f;
+		
+			if (stream.CanWrite) {
+				writer.WriteLine (JsonUtility.ToJson (player));
+				writer.Flush ();
+			}
+			if (stream.CanRead) {
+				string input = reader.ReadLine ();
+				Debug.Log ("input "+input);
+				Player p = JsonUtility.FromJson<Player>(input);
+				Debug.Log (p.name);
+			}				
+
+			s = 0;
 		}
+		s++;
 	}
+}
+
+public class Player{
+
+	public float x,y,z;
+	public string name;
 
 }
