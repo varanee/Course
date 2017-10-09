ebd78d9e1da4f1b599d1013f8e174b8a881d1bae
Ekarat Rattagan <pokekarat@gmail.com>
2017-10-09 21:59:46 +0700

    Update Server.py

--- Game/Week9_Client_Server/Server.py
+++ Game/Week9_Client_Server/Server.py
@@ -1,24 +1,47 @@
-""" 
-A simple echo server,
-to test tcp networking code
-""" 
-
-import socket 
+import socket
+import thread
+import errno
+import pickle
 
 host = '' 
 port = 16000
 backlog = 5 
-size = 1024 
+size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host,port)) 
+s.bind((host,port))
 s.listen(backlog) 
+list = []
+
+
+def talk(client, list):
+    while 1:
+        data = client.recv(size)
+        print "receive = " + data
+        for c in list:
+           client.sendto(data, (c[0],c[1]))
+
+    s.close()
 
 while 1:
-    client, address = s.accept() 
-    print "Client connected."
-    client.send("Hello!\n") 
 
-    while 1: 
-        data = client.recv(size)
-        print "receive = "+data
+    print "Waiting for some clients ..."
+    client, address = s.accept()
+
+    print "Client connected."
+    print address
+
+    list.append(address)
+
+    print len(list)
+
+    client.send("Hello!\n")
+
+    try:
+
+        thread.start_new_thread(talk,(client,list, ))
+
+    except SocketError as e:
+        if e.errno != errno.ECONNRESET:
+            raise  # Not error we are looking for
+        pass  # Handle error here.
