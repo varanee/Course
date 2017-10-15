94029874334caf4fa0bd4392c0f0026849d728b7
Ekarat Rattagan <pokekarat@gmail.com>
2017-10-15 23:22:53 +0700

    Update Server.py

--- Game/Week9_Client_Server/Server.py
+++ Game/Week9_Client_Server/Server.py
@@ -5,6 +5,7 @@ import errno
 import pickle
 import time
 import inspect
+import struct
 
 host = ''
 port = 16000
@@ -12,32 +13,47 @@ backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host,port))
+s.bind((host, port))
 s.listen(backlog)
 list = []
 
 
 def talk(client, list):
     lock = thread.allocate_lock()
-    while 1:
-        lock.acquire()
+    try:
+        while 1:
+            lock.acquire()
+            data = client.recv(size)
+            print "Send = " + data + " to "
+            for c in list:
+                c.send(data)
+            lock.release()
+            print "Release lock"
+    except:
+        list.remove(client)
+        handler(client, address)
+    #finally:
+    #    s.close()
+    #    lock.acquire()
+    #    list.remove(client)
+    #    lock.release()
+    #    print "sock close "+str(len(list))
 
-        data = client.recv(size)
-        print "Send = " + data + " to "
-        for c in list:
-            c.send(data)
-        lock.release()
-        print "Release lock"
-
-    s.close()
 #threads = []
+def handler(client_sock, addr):
+    try:
+        print('new client from %s:%s' % addr)
+        time.sleep(1)
+    finally:
+        client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER,
+            struct.pack('ii', 1, 0))
+        client_sock.close()   # close current connection directly
 
 
 while 1:
 
     print "Waiting for some clients ..."
     client, address = s.accept()
-
     print "Client connected."
     print address
 
@@ -50,7 +66,4 @@ while 1:
     t = threading.Thread(target=talk, args=(client,list, ))
    # threads.append(t)
     t.start()
-
-
-
-
+    
