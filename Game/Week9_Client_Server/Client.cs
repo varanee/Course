6c39461fef4c55c4f9af944db5675b677b6f2035
Ekarat Rattagan <pokekarat@gmail.com>
2017-10-24 17:23:37 +0700

    Update Client.cs

--- Game/Week9_Client_Server/Client.cs
+++ Game/Week9_Client_Server/Client.cs
@@ -1,133 +1,50 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System;
-using System.IO;
 
+public class LerpExample : MonoBehaviour {
+	//float lerpTime = 1f;
+	float currentLerpTime;
+	float moveDistance = 5f;
 
-public class Main : MonoBehaviour
-{
-	public GameObject go;
-	public GameObject me;
-	LinkedList<GameObject> listOthers;
-	LinkedList<string> listOtherNames;
-	StreamWriter writer;
-	StreamReader reader;
-	NetworkStream stream;
-	Player player;
-	TcpClient client;
+	Vector3 startPos;
+	Vector3 endPos;
+	public static Vector3 POS;
+	float speed = 5f;
 
-	// Use this for initialization
-	void Start ()
-	{
-		listOthers = new LinkedList<GameObject> ();
-		listOtherNames = new LinkedList<String> ();
-
-		player = new Player ();
-		player.n = "pok1";
-		player.a = 1;
-
-		print ("Connection");
-
-		client = new TcpClient ("127.0.0.1", 16000);
-
-		stream = client.GetStream ();
-		stream.ReadTimeout = 10;
-		if (stream.CanRead) {
-			writer = new StreamWriter (stream);
-			reader = new StreamReader (stream);
-			Debug.Log (reader.ReadLine ());
-		}
+	protected void Start() {
+		startPos = transform.position;
+		endPos = transform.position + transform.up * moveDistance;
 	}
 
-	//60 fps
-	int s = 0;
-	void FixedUpdate ()
-	{
-		try {
-			
-			player.x = Mathf.Round(LerpExample.POS.x);
-			player.y = Mathf.Round(LerpExample.POS.y);
-
-			//Send data to server every 1 second.
-			if (s % 60 == 0)
-			{
-				s = 0;
-				if (stream.CanWrite) {
-					writer.WriteLine (JsonUtility.ToJson (player));
-					writer.Flush ();
-				}
-			} 
-			else 
-			{
-				s++;
-			}
-
-			if (stream.CanRead) {
-				string input = reader.ReadLine ();
-				reader.DiscardBufferedData();
-				//Debug.Log (input);
-				Player p = JsonUtility.FromJson<Player> (input);
-				manageOtherPlayers (p);
-			}
-		} 
-		catch (Exception e) {
-			Debug.Log (e.ToString ());
+	protected void Update() {
+		//reset when we press spacebar
+		/*if (Input.GetKeyDown(KeyCode.Space)) {
+			currentLerpTime = 0f;
 		}
+
+		//increment timer once per frame
+		currentLerpTime += Time.deltaTime;
+		if (currentLerpTime > lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+
+		//lerp!
+		float perc = currentLerpTime / lerpTime;
+		transform.position = Vector3.Lerp(startPos, endPos, perc);
+		*/
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate (Vector3.up * speed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate (Vector3.down * speed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+
+		POS = transform.position;
 	}
-
-	void manageOtherPlayers (Player p)
-	{
-		//Debug.Log (p.n + " " + p.a);
-
-		if (!listOtherNames.Contains (p.n)) 
-		{ 
-			
-			listOtherNames.AddLast (p.n);
-			GameObject newGo = (GameObject)Instantiate (go, new Vector3 (p.x, p.y, p.z), Quaternion.identity);
-			newGo.name = p.n;
-			listOthers.AddLast (newGo);
-
-		} else { //Update their status 
-			
-			GameObject.Find(p.n).transform.position = new Vector3(p.x,p.y,p.z);
-			Debug.Log (p.x + " " + p.y);
-
-		}
-	}
-
-	void OnGUI ()
-	{
-		if (listOthers.Count > 0) {
-			foreach (GameObject g in listOthers) {
-				Vector3 position = Camera.main.WorldToScreenPoint (g.transform.position);
-				Vector2 textSize = GUI.skin.label.CalcSize (new GUIContent (g.name));
-				GUI.Label (new Rect (position.x, Screen.height - (position.y + 40f), textSize.x, textSize.y), g.name);
-			}
-		}
-	}
-
-	void OnApplicationQuit()
-	{
-		try
-		{
-			client.Close();
-		}
-		catch(Exception e)
-		{
-			Debug.Log(e.Message);
-		}
-	}
-}
-
-public class Player
-{
-
-	public float x, y, z;
-	public string n;
-	public int a;
-
 }
