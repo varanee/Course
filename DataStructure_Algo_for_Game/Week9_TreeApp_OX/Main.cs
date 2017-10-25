1b9381e386aba7bc81b0b10e687246dc8e11f6f2
Ekarat Rattagan <pokekarat@gmail.com>
2017-10-25 16:22:26 +0700

    Update Main.cs

--- DataStructure_Algo_for_Game/Week9_TreeApp_OX/Main.cs
+++ DataStructure_Algo_for_Game/Week9_TreeApp_OX/Main.cs
@@ -4,34 +4,39 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-public class Main : MonoBehaviour
-{
+public class tree : MonoBehaviour {
 	string output = "";
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		//1 = o, 2 = x
 		char playerTurn = '1';
 		TreeNode<string> root = new TreeNode<string> ("121212000");
 		int numOfBlankCell = 3;
-		generateTree (root, root.Data, playerTurn, numOfBlankCell);
-	
+
+		generateTree (root, playerTurn, numOfBlankCell);
+		//check the correctness of the first pattern
+		bool result = checkGameStatus("111000000"); //true
+		bool result2 = checkGameStatus("222000000"); //true
+		bool result3 = checkGameStatus("112000000"); //False
+
 		//Show output
+		int count = 0;
 		foreach (TreeNode<string> node in root) {
 			string indent = CreateIndent (node.Level);
 			output += indent + (node.Data + "\n" ?? "null\n");
+			count++;
 		}
-
+		Debug.Log ("Num of nodes = " + count);
 		Debug.Log (output);
 	}
 
-	void generateTree(TreeNode<string> treeNode, string value, char player, int numChild)
+	void generateTree(TreeNode<string> treeNode, char player, int numChild)
 	{
 		int countZero = 0;
 		for (int i = 0; i < numChild; i++) 
 		{
-			StringBuilder _value = new StringBuilder (value);
-			for (int j = countZero; j < value.Length; j++) 
+			StringBuilder _value = new StringBuilder (treeNode.Data);
+			for (int j = countZero; j < _value.Length; j++) 
 			{
 				char cell = _value [j];
 				if (cell == '0') 
@@ -40,11 +45,34 @@ public class Main : MonoBehaviour
 					_value.Replace (cell, player, j, 1);
 					TreeNode<string> _treeNode = treeNode.AddChild (_value.ToString ());
 					char _player = (player == '1' ? '2' : '1');
-					generateTree (_treeNode, _treeNode.Data, _player, numChild - 1);
+
+					//Check Win/Lose
+
+					generateTree (_treeNode, _player, numChild - 1);
 					break;
 				}
 			}
 		}
+	}
+	//If true is game over
+	public bool checkGameStatus(string treeNodeValue){
+		
+		string firstRow = treeNodeValue.Substring (0, 3);
+		if (firstRow.Equals ("111") || firstRow.Equals ("222")) {
+			return true;
+		}
+
+		string secondRow = treeNodeValue.Substring (3, 3);
+		if (firstRow.Equals ("111") || firstRow.Equals ("222")) {
+			return true;
+		}
+
+		string thirdRow = treeNodeValue.Substring (6, 3);
+		if (firstRow.Equals ("111") || firstRow.Equals ("222")) {
+			return true;
+		}
+			
+		return false;
 	}
 
 	public Vector2 scrollPosition = Vector2.zero;
@@ -57,48 +85,62 @@ public class Main : MonoBehaviour
 		GUILayout.EndScrollView();
 	}
 
-	private static String CreateIndent (int depth)
-	{
-		StringBuilder sb = new StringBuilder ();
-		for (int i = 0; i < depth; i++) {
-			sb.Append ("    ");
+	//iterative
+	int Factorial(int i){
+		int ret = i;
+		for (int x = i-1; x >= 1; x--) {
+			ret = ret * x; 
 		}
-		return sb.ToString ();
+		return ret;
+	}
+
+	//Recursive
+	int Factorial2(int i){
+		if (i == 1)
+			return i;
+		else
+			return Factorial2 (i-1) * i;
+	}
+
+	private static String CreateIndent(int depth)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < depth; i++)
+		{
+			sb.Append("    ");
+		}
+		return sb.ToString();
 	}
 }
 
 
 /*
- 	TreeNode<string> node20 = node2.AddChild(null);
-				TreeNode<string> node21 = node2.AddChild("node21");
-					TreeNode<string> node210 = node21.AddChild("node210");
-					TreeNode<string> node211 = node21.AddChild("node211");
-			TreeNode<string> node3 = root.AddChild("node3");
-				TreeNode<string> node30 = node3.AddChild("node30");
+     TreeNode<string> node20 = node2.AddChild(null);
+                TreeNode<string> node21 = node2.AddChild("node21");
+                    TreeNode<string> node210 = node21.AddChild("node210");
+                    TreeNode<string> node211 = node21.AddChild("node211");
+            TreeNode<string> node3 = root.AddChild("node3");
+                TreeNode<string> node30 = node3.AddChild("node30");
 */
 
 public class TreeNode<T> : IEnumerable<TreeNode<T>>
 {
 	public T Data { get; set; }
-
 	public TreeNode<T> Parent { get; set; }
-
 	public ICollection<TreeNode<T>> Children { get; set; }
 
-	public TreeNode (T data)
-	{
+	public TreeNode(T data){
 		this.Data = data;
-		this.Children = new LinkedList<TreeNode<T>> ();
+		this.Children = new LinkedList<TreeNode<T>>();
 	}
 
-	public TreeNode<T> AddChild (T child)
-	{
-		TreeNode<T> childNode = new TreeNode<T> (child) { Parent = this };
-		this.Children.Add (childNode);
+	public TreeNode<T> AddChild(T child)    {
+		TreeNode<T> childNode = new TreeNode<T>(child) { Parent = this };
+		this.Children.Add(childNode);
 		return childNode;
 	}
 
-	public Boolean IsRoot {
+	public Boolean IsRoot    {
 		get { return Parent == null; }
 	}
 
@@ -114,21 +156,21 @@ public class TreeNode<T> : IEnumerable<TreeNode<T>>
 		}
 	}
 
-	public override string ToString ()
-	{
-		return Data != null ? Data.ToString () : "[data null]";
+	public override string ToString(){
+		return Data != null ? Data.ToString() : "[data null]";
 	}
 
 
-	IEnumerator IEnumerable.GetEnumerator ()
+	IEnumerator IEnumerable.GetEnumerator()
 	{
-		return GetEnumerator ();
+		return GetEnumerator();
 	}
 
-	public IEnumerator<TreeNode<T>> GetEnumerator ()
+	public IEnumerator<TreeNode<T>> GetEnumerator()
 	{
 		yield return this;
-		foreach (var directChild in this.Children) {
+		foreach (var directChild in this.Children)
+		{
 			foreach (var anyChild in directChild)
 				yield return anyChild;
 		}
