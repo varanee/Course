2477e05e8a608b42d0d528444ec59c8b5c6c6593
pokekarat <pokekarat@gmail.com>
2018-01-20 07:28:47 +0700

    Network Terrain

---
+++ Game/Network/Network1/Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		float z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate (0, x, 0);
+		transform.Translate (0, 0, z);
+	}
+}
