6d7ee717a2d220c26035ae807426a43cbc66e1ef
pokekarat <pokekarat@gmail.com>
2018-01-21 22:51:42 +0700

    Update network unity

--- Game/Network/Network1/Assets/PlayerController.cs
+++ Game/Network/Network1/Assets/PlayerController.cs
@@ -1,19 +1,53 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour
+{
+	private Camera cam;
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+		cam  = Camera.main;
+
+		cam.transform.position = transform.position - new Vector3(0,0,-5f);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
-		float z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate (0, x, 0);
-		transform.Translate (0, 0, z);
+
+	void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);        
+	}
+
+	public override void OnStartLocalPlayer ()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
 	}
 }
